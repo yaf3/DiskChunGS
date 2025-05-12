1e3bfb9ce62362e7e7c8e2d4bf6c8bdbffbf1d3c
casimirfeldmann <casimir.feldmann@gmail.com>
2025-05-12 23:34:29 +0200

    cleanup

--- run.sh
+++
@@ -1,9 +0,0 @@
-bin/replica_rgbd \
-    slam_deps/ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    cfg/ORB_SLAM3/RGB-D/Replica/office0.yaml \
-    cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    /data/Replica/office0 \
-    results/test
-
-
-python3 python/eval.py --dataset_center_path /data --result_main_folder /workspaces/cartgs/results --exp_name test
