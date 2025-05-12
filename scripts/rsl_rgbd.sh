408984acd86fb95e775e382bbe96d3f2340fc5ac
casimirfeldmann <casimir.feldmann@gmail.com>
2025-05-12 08:02:51 +0000

    add other scenes to rsl_rgbd script

--- scripts/rsl_rgbd.sh
+++ scripts/rsl_rgbd.sh
@@ -33,24 +33,46 @@ do
     echo "Completed trial $((i+1)) of $num_trials"
 done
 
-# for ((i=0; i<num_trials; i++))
-# do
-#     echo "Starting trial $((i+1)) of $num_trials"
+for ((i=0; i<num_trials; i++))
+do
+    echo "Starting trial $((i+1)) of $num_trials"
     
-#     # Use the direct path to the executable
-#     /root/catkin_ws/devel/lib/lsgs_ros/lsgs_ros_node \
-#         __name:=gaussian_slam \
-#         _vocabulary_path:=/workspaces/large_scale_gaussian_slam/third_party/ORB-SLAM3/Vocabulary/ORBvoc.txt \
-#         _orb_settings_path:=/workspaces/large_scale_gaussian_slam/cfg/ORB_SLAM3/RGB-D/RSL/arche_train1.yaml \
-#         _gaussian_settings_path:=/workspaces/large_scale_gaussian_slam/cfg/gaussian_mapper/RGB-D/RSL/arche_train2.yaml \
-#         _output_directory:=/workspaces/large_scale_gaussian_slam/results/${exp_name}/RSL_rgbd_$i/train1  \
-#         _use_viewer:=true \
-#         _mode:=rgbd \
-#         _rgb_topic:=/left_camera_rgb \
-#         _depth_topic:=/zed2/zed_node/depth/depth_registered \
-#         _slam_mode:=external \
-#         _target_frame:=map \
-#         _source_frame:=zed2_left_camera_optical_frame
+    # Use the direct path to the executable
+    /root/catkin_ws/devel/lib/lsgs_ros/lsgs_ros_node \
+        __name:=gaussian_slam \
+        _vocabulary_path:=/workspaces/large_scale_gaussian_slam/third_party/ORB-SLAM3/Vocabulary/ORBvoc.txt \
+        _orb_settings_path:=/workspaces/large_scale_gaussian_slam/cfg/ORB_SLAM3/RGB-D/RSL/arche_train1.yaml \
+        _gaussian_settings_path:=/workspaces/large_scale_gaussian_slam/cfg/gaussian_mapper/RGB-D/RSL/arche_train2.yaml \
+        _output_directory:=/workspaces/large_scale_gaussian_slam/results/${exp_name}/RSL_rgbd_$i/train2  \
+        _use_viewer:=true \
+        _mode:=rgbd \
+        _rgb_topic:=/left_camera_rgb \
+        _depth_topic:=/zed2/zed_node/depth/depth_registered \
+        _slam_mode:=external \
+        _target_frame:=map \
+        _source_frame:=zed2_left_camera_optical_frame
     
-#     echo "Completed trial $((i+1)) of $num_trials"
-# done
+    echo "Completed trial $((i+1)) of $num_trials"
+done
+
+for ((i=0; i<num_trials; i++))
+do
+    echo "Starting trial $((i+1)) of $num_trials"
+    
+    # Use the direct path to the executable
+    /root/catkin_ws/devel/lib/lsgs_ros/lsgs_ros_node \
+        __name:=gaussian_slam \
+        _vocabulary_path:=/workspaces/large_scale_gaussian_slam/third_party/ORB-SLAM3/Vocabulary/ORBvoc.txt \
+        _orb_settings_path:=/workspaces/large_scale_gaussian_slam/cfg/ORB_SLAM3/RGB-D/RSL/arche_train1.yaml \
+        _gaussian_settings_path:=/workspaces/large_scale_gaussian_slam/cfg/gaussian_mapper/RGB-D/RSL/arche_gazebo1.yaml \
+        _output_directory:=/workspaces/large_scale_gaussian_slam/results/${exp_name}/RSL_rgbd_$i/gazebo1  \
+        _use_viewer:=true \
+        _mode:=rgbd \
+        _rgb_topic:=/left_camera_rgb \
+        _depth_topic:=/zed2/zed_node/depth/depth_registered \
+        _slam_mode:=external \
+        _target_frame:=map \
+        _source_frame:=zed2_left_camera_optical_frame
+    
+    echo "Completed trial $((i+1)) of $num_trials"
+done
