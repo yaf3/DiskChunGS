aeb85a49a83f8c0505809e193adb25ca912a6acc
casimirfeldmann <casimir.feldmann@gmail.com>
2025-06-24 07:23:03 +0000

    fix orbslam nan issues

--- slam_deps/ORB-SLAM3/src/Sim3Solver.cc
+++ slam_deps/ORB-SLAM3/src/Sim3Solver.cc
@@ -182,7 +182,7 @@ Eigen::Matrix4f Sim3Solver::iterate(int nIterations,
       vAvailableIndices.pop_back();
     }
 
-    ComputeSim3(P3Dc1i, P3Dc2i);
+    if (!ComputeSim3(P3Dc1i, P3Dc2i)) continue;
 
     CheckInliers();
 
@@ -251,7 +251,7 @@ Eigen::Matrix4f Sim3Solver::iterate(int nIterations,
       vAvailableIndices.pop_back();
     }
 
-    ComputeSim3(P3Dc1i, P3Dc2i);
+    if (!ComputeSim3(P3Dc1i, P3Dc2i)) continue;
 
     CheckInliers();
 
@@ -293,7 +293,7 @@ void Sim3Solver::ComputeCentroid(Eigen::Matrix3f &P,
   for (int i = 0; i < P.cols(); i++) Pr.col(i) = P.col(i) - C;
 }
 
-void Sim3Solver::ComputeSim3(Eigen::Matrix3f &P1, Eigen::Matrix3f &P2) {
+bool Sim3Solver::ComputeSim3(Eigen::Matrix3f &P1, Eigen::Matrix3f &P2) {
   // Custom implementation of:
   // Horn 1987, Closed-form solution of absolute orientataion using unit
   // quaternions
@@ -345,12 +345,13 @@ void Sim3Solver::ComputeSim3(Eigen::Matrix3f &P1, Eigen::Matrix3f &P2) {
 
   Eigen::Vector3f vec = evec.block<3, 1>(
       1, maxIndex);  // extract imaginary part of the quaternion (sin*axis)
+  double vecNorm = vec.norm();
+  if (vecNorm == 0) return false;
 
   // Rotation angle. sin is the norm of the imaginary part, cos is the real part
-  double ang = atan2(vec.norm(), evec(0, maxIndex));
+  double ang = atan2(vecNorm, evec(0, maxIndex));
 
-  vec = 2 * ang * vec /
-        vec.norm();  // Angle-axis representation. quaternion angle is the half
+  vec = 2 * ang * vec / vecNorm;  // Angle-axis representation. quaternion angle is the half
   mR12i = Sophus::SO3f::exp(vec).matrix();
 
   // Step 5: Rotate set 2
@@ -393,6 +394,7 @@ void Sim3Solver::ComputeSim3(Eigen::Matrix3f &P1, Eigen::Matrix3f &P2) {
 
   Eigen::Vector3f tinv = -sRinv * mt12i;
   mT21i.block<3, 1>(0, 3) = tinv;
+  return true;
 }
 
 void Sim3Solver::CheckInliers() {
