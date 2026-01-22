d8fa4861394fd5f9efb279c2b427eb8fe74a1f65
casimirfeldmann <casimir.feldmann@gmail.com>
2026-01-22 09:23:36 +0000

    change script permission & make all.sh fit paper benchmarks

--- scripts/all.sh
+++ scripts/all.sh
@@ -2,8 +2,6 @@
 exp=$1
 num_trials=$2
 
+scripts/kitti_stereo.sh $exp $num_trials
 scripts/replica_mono.sh $exp $num_trials
-scripts/replica_rgbd.sh $exp $num_trials
-
 scripts/tum_mono.sh $exp $num_trials
-scripts/tum_rgbd.sh $exp $num_trials
