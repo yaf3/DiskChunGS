1ad40e6e9fef06388af526cc3e01619cd8deaf70
casimirfeldmann <casimir.feldmann@gmail.com>
2026-03-08 11:03:05 +0000

    remove unused scripts

--- scripts/colmap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bin/train_colmap \
-    cfg/colmap/gaussian_splatting.yaml \
-    ./data/tandt_db/db/drjohnson \
-    results/colmap/drjohnson \
-    no_viewer
--- scripts/realsense_d455.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bin/realsense_rgbd \
-    slam_deps/ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    cfg/ORB_SLAM3/RGB-D/RealCamera/realsense_d455_rgbd.yaml \
-    cfg/gaussian_mapper/RGB-D/RealCamera/realsense_rgbd.yaml \
-    results/realsense_d455_rgbd
